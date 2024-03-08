9a70024271d99d172417d7ad0ef570c496092f30
csortansz <csortansz@innomatrix.hu>
2024-03-08 22:27:53 +0100

    learning react routes

---
+++ src/routing/UserDetail.tsx
@@ -0,0 +1,10 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
+const UserDetail = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  return <p>User {params.id} </p>;
+};
+
+export default UserDetail;
